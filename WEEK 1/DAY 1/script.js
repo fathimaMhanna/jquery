3cefb50f3e3a7c131ad364825cbbddad164529db
unknown <fathimahanna8250@gmail.com>
2026-05-22 10:11:01 +0530

    jquery intro , events

---
+++ WEEK 1/DAY 1/script.js
@@ -0,0 +1,16 @@
+$('#text').css('color', 'red');
+$('.box').css('backgroundColor', 'green');
+$('p').css({
+    'color': 'blue',
+    'fontSize': '20px',
+    'backgroundColor': 'yellow',
+    'padding': '20px'
+})
+$('.box1').css({
+    'height':'100px',
+    'width':'100px',
+    'backgroundColor':'yellow',
+    'color':'red',
+    'display':'flex'
+    
+})
